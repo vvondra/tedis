45542c5a7f75b819317c9ca9d4ad1960ff15ec81
Vojtech Vondra <vojtavondra@gmail.com>
2017-06-19 20:36:08 +0200

    Upgrade deps for React 16
    Former-commit-id: f2476e62cea5f4933de6a575f02d18da1ceb724f

--- client/windows/MainWindow/components/InstanceContent/components/DatabaseContainer/components/Content/components/KeyContent/components/SortHeaderCell.jsx
+++ client/windows/MainWindow/components/InstanceContent/components/DatabaseContainer/components/Content/components/KeyContent/components/SortHeaderCell.jsx
@@ -1,7 +1,7 @@
 'use strict'
 
 import React, {PureComponent} from 'react'
-import {Cell} from 'fixed-data-table-contextmenu'
+import {Cell} from 'fixed-data-table-2'
 
 export default class SortHeaderCell extends PureComponent {
   handleClick(evt) {
